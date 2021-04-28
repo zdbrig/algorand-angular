4ef7b9f7b49806ed738f98514491da5443b57048
zdbrig <bacem.bergaoui@gmail.com>
2021-04-28 14:37:33 +0100

    add algorand integration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bacem';
+  algosigned = "--";
+  algoConnected = "--";
+
+  checkAlgo = () => {
+    // @ts-ignore
+    if (typeof AlgoSigner !== 'undefined') {
+      this.algosigned = 'AlgoSigner is installed.';
+    } else {
+      this.algosigned = 'AlgoSigner is NOT installed.';
+    }
+
+  }
+
+  connectAlgo = () => {
+    // @ts-ignore
+    AlgoSigner.connect()
+      .then((d) => {
+        this.algoConnected = JSON.stringify(d, null, 2);
+        // @ts-ignore
+        AlgoSigner.accounts({
+          ledger: 'MainNet'
+        })
+          .then((d) => {
+            this.algoConnected = JSON.stringify(d, null, 2);
+          })
+
+      })
+      .catch((e) => {
+        this.algoConnected = JSON.stringify(e, null, 2);
+      });
+
+  }
 }
