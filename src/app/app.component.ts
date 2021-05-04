98207faa80517b86d9bac67b7f678059aeaa4a79
zdbrig <bacem.bergaoui@gmail.com>
2021-05-04 02:24:44 +0100

    add pay and create asset examples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { StringifyOptions } from 'querystring';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +11,138 @@ export class AppComponent {
   title = 'bacem';
   algosigned = "--";
   algoConnected = "--";
+  connected = false;
+  accounts = [];
+  selectedAccount = "";
+  paymentForm: FormGroup;
+  createAssetForm: FormGroup;
+
+  signedTx: any;
+  ledger: string;
+  lastmessage: string;
+  txId: string;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.paymentForm = this.formBuilder.group({
+      from: new FormControl(""),
+      to: new FormControl(""),
+      amount: new FormControl(""),
+    });
+
+    this.createAssetForm = this.formBuilder.group({
+      from: new FormControl(""),
+      name: new FormControl(""),
+      count: new FormControl(""),
+    });
+
+  }
+
+  pay = () => {
+    // @ts-ignore
+    AlgoSigner.algod({
+      ledger: this.ledger,
+      path: '/v2/transactions/params'
+    })
+      .then((d) => {
+        let txParams = d;
+        // @ts-ignore
+        AlgoSigner.sign({
+          from: this.paymentForm.value.from,
+          to: this.paymentForm.value.to,
+          amount: +this.paymentForm.value.amount,
+          note: "",
+          type: 'pay',
+          fee: txParams['min-fee'],
+          firstRound: txParams['last-round'],
+          lastRound: txParams['last-round'] + 1000,
+          genesisID: txParams['genesis-id'],
+          genesisHash: txParams['genesis-hash'],
+          flatFee: true
+        })
+          .then((signedTx) => {
+            this.lastmessage = "Transaction signed .. sending transaction";
+            // @ts-ignore
+            AlgoSigner.send({
+              ledger: this.ledger,
+              tx: signedTx.blob
+            })
+              .then((d) => {
+                this.signedTx = d;
+                this.lastmessage = " Transaction sent : " + JSON.stringify(d);
+                this.txId = d.txId;
+              })
+              .catch((e) => {
+
+                this.lastmessage = " Transaction Failed : " + JSON.stringify(e);
+              });
+          })
+          .catch((e) => {
+            this.lastmessage = " Transaction Failed : " + JSON.stringify(e);
+
+          });
+      })
+      .catch((e) => {
+        this.lastmessage = " Transaction Failed : " + JSON.stringify(e);
+
+      });
+
+
+  }
+
+  createAsset = () => {
+    // @ts-ignore
+    AlgoSigner.algod({
+      ledger: this.ledger,
+      path: '/v2/transactions/params'
+    })
+      .then((d) => {
+        let txParams = d;
+        // @ts-ignore
+        AlgoSigner.sign({
+
+          from: this.createAssetForm.value.from,
+          assetName: this.createAssetForm.value.name,
+          assetUnitName: this.createAssetForm.value.name,
+          assetTotal: +this.createAssetForm.value.count,
+          assetDecimals: +0,
+          note: "",
+          type: 'acfg',
+          fee: txParams['min-fee'],
+          firstRound: txParams['last-round'],
+          lastRound: txParams['last-round'] + 1000,
+          genesisID: txParams['genesis-id'],
+          genesisHash: txParams['genesis-hash'],
+          flatFee: true
+        })
+          .then((signedTx) => {
+            this.lastmessage = "Transaction signed .. sending transaction";
+            // @ts-ignore
+            AlgoSigner.send({
+              ledger: this.ledger,
+              tx: signedTx.blob
+            })
+              .then((d) => {
+                this.signedTx = d;
+                this.lastmessage = " Transaction sent : " + JSON.stringify(d);
+                this.txId = d.txId;
+              })
+              .catch((e) => {
+
+                this.lastmessage = " Transaction Failed : " + JSON.stringify(e);
+              });
+          })
+          .catch((e) => {
+            this.lastmessage = " Transaction Failed : " + JSON.stringify(e);
+
+          });
+      })
+      .catch((e) => {
+        this.lastmessage = " Transaction Failed : " + JSON.stringify(e);
+
+      });
+
+
+  }
 
   checkAlgo = () => {
     // @ts-ignore
@@ -20,17 +154,23 @@ export class AppComponent {
 
   }
 
-  connectAlgo = () => {
+  selectAccount = (a) => {
+    this.selectedAccount = a;
+  }
+
+  connectAlgo = (ledger: string) => {
     // @ts-ignore
     AlgoSigner.connect()
       .then((d) => {
         this.algoConnected = JSON.stringify(d, null, 2);
         // @ts-ignore
         AlgoSigner.accounts({
-          ledger: 'MainNet'
+          ledger: ledger
         })
           .then((d) => {
-            this.algoConnected = JSON.stringify(d, null, 2);
+            this.accounts = d;
+            this.connected = true;
+            this.ledger = ledger;
           })
 
       })
